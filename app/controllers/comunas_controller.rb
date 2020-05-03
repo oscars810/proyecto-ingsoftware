0fd02e4dc5b2f151794dde07abec785e71a6234c
Eduardo <evasqueztdev@protonmail.com>
2020-05-03 00:23:12 -0400

    Comunas implementado

--- app/controllers/comunas_controller.rb
+++ app/controllers/comunas_controller.rb
@@ -1,13 +1,56 @@
 class ComunasController < ApplicationController
+
+  layout "admin"
+
+  # Create
   def new
+    @comuna = Comuna.new
   end
 
+  def create
+    comuna_params = params.require(:comuna).permit(:nombre)
+    @comuna = Comuna.create(comuna_params)
+
+    if @comuna.save
+      redirect_to comunas_path, notice: 'Comuna creada con éxito'
+    else
+      redirect_to comunas_new_path, notice: 'Ocurrió un error al crear la película'
+    end
+  end
+
+  # Read
+
   def index
+    @comunas = Comuna.all
   end
 
   def show
+    @comuna = Comuna.find(params[:id])
   end
 
+  # Update
+
   def edit
+    @comuna = Comuna.find(params[:id])
   end
+
+  def update
+    comuna_params = params.require(:comuna).permit(:nombre)
+    @comuna = Comuna.find(params[:id])
+
+    if @comuna.update(comuna_params)
+      redirect_to comuna_path(@comuna.id), notice: 'Comuna editada con éxito'
+    else
+      redirect_to comunas_new_path, notice: 'Ocurrió un error al editar la película'
+    end
+  end
+
+  # Delete
+
+  def destroy
+    @comuna = Comuna.find(params[:id])
+    @comuna.destroy
+    redirect_to comunas_path, notice: 'Comuna eliminada con éxito'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
-  get 'comunas/new'
-  get 'comunas/index'
-  get 'comunas/show'
-  get 'comunas/edit'
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  # CRUD de Comunas
+  # Create
+  get 'comunas/new', to: 'comunas#new'
+  post 'comunas', to: 'comunas#create'
+
+  # Read
+  get 'comunas', to: 'comunas#index'
+  get 'comunas/:id', to: 'comunas#show', as: :comuna
+
+  # Update
+  get 'comunas/:id/edit', to: 'comunas#edit', as: :comunas_edit
+  patch 'comunas/:id/', to: 'comunas#update'
+  put 'comunas/:id/', to: 'comunas#update'
+
+  # Destroy
+  delete 'comunas/:id', to: 'comunas#destroy'
+
+
 end
