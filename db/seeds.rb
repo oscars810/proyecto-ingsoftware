118d4e40791ee9f4a6b4556bb722d91e4c21e8f6
BENITO TONDREAUX <benito.tondreaux@uc.cl>
2020-05-03 01:16:20 -0400

    agregando seeds de usuarios y locales

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.new(email: 'hackerman@uc.cl', password: 'hackerman', password_confirmation: 'hackerman', admin: true, nombre: 'Admin')
+#user = User.new(email: 'hackerman@uc.cl', password: 'hackerman', password_confirmation: 'hackerman', admin: true, nombre: 'Admin')
+#user.save!
+
+user = User.new(email: 'oscar@uc.cl', password: 'oscars810', password_confirmation: 'oscars810', admin: false, nombre: 'Oscar')
 user.save!
+
+user = User.new(email: 'benito1234@uc.cl', password: '1junio1996', password_confirmation: '1junio1996', admin: false, nombre: 'Benito')
+user.save!
+
+user = User.new(email: 'eduardo1234@uc.cl', password: 'eduardo1234', password_confirmation: 'eduardo1234', admin: false, nombre: 'Eduardo')
+user.save!
+
+user = User.new(email: 'felipe1234@uc.cl', password: 'felipe1234', password_confirmation: 'felipe1234', admin: false, nombre: 'Felipe')
+user.save!
+
+local = Local.new(nombre: 'Juan y Medio', descripcion: 'Restaurante de comida tipica chilena', aceptado: true, idcomuna: 1, idusuario: 2)
+local.save!
+
+local = Local.new(nombre: 'Pamplona', descripcion: 'Sandwicheria española', aceptado: true, idcomuna: 3, idusuario: 1)
+local.save!
+
+local = Local.new(nombre: 'Taiko', descripcion: 'Tenedor libre de sushi', aceptado: false, idcomuna: 3, idusuario: 4)
+local.save!
