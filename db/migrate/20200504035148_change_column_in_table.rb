2581855cd2199104bd333c4953938b1179af7f75
oscars810 <oscar.carcamoz@uc.cl>
2020-05-04 02:30:54 -0400

    Revert "Formulario terminado pero con posibles futuras fallas en la seleccion de la comuna"
    This reverts commit 0131f50c2c49d69b420a85ae480145279be7214b.

--- db/migrate/20200504035148_change_column_in_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnInTable < ActiveRecord::Migration[5.2]
-  def change
-    change_column(:locals, :aceptado, :boolean, default: false)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_035148) do
+ActiveRecord::Schema.define(version: 2020_05_03_042617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_035148) do
   create_table "locals", force: :cascade do |t|
     t.string "nombre"
     t.string "descripcion"
-    t.boolean "aceptado", default: false
+    t.boolean "aceptado"
     t.integer "idusuario"
     t.integer "idcomuna"
     t.datetime "created_at", null: false
