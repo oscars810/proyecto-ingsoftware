8123d30021cfbb713e4aa2c2784ec98efe169415
oscars810 <oscar.carcamoz@uc.cl>
2020-05-03 23:07:50 -0400

    Registro casi listo

--- app/controllers/locals_controller.rb
+++ app/controllers/locals_controller.rb
@@ -1,5 +1,6 @@
 class LocalsController < ApplicationController
   def new
+    @local = Local.new
   end
 
   def show
@@ -9,5 +10,16 @@ class LocalsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    l_params = params.require(:local).permit(:nombre, :comuna, :descripcion)
+    @local = Local.create(l_params)
+
+    if @local.save
+      redirect_to new_path, notice: 'Local agregado de pana'
+    else
+      redirect_to new_path, notice: 'Falla en la agregacion del local'
+    end
   end
 end
