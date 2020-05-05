8823605606df20d9428a6e832cea45f7a2adb7d0
oscars810 <oscar.carcamoz@uc.cl>
2020-05-04 23:09:04 -0400

    Arreglando error merge

--- db/migrate/20200504035148_change_column_in_table.rb
+++ db/migrate/20200504035148_change_column_in_table.rb
@@ -1,5 +1,5 @@
 class ChangeColumnInTable < ActiveRecord::Migration[5.2]
   def change
-    change_column(:locals, :aceptado, :boolean, default: false)
+    change_column(:locals, :aceptado, :boolean, default: false) 
   end
 end
