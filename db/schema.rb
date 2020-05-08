0e084de4a4f4cb5699dc30ed0056930e7efa2869
Eduardo <evasqueztdev@protonmail.com>
2020-05-08 10:51:00 -0400

    Fix valoracion en comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_025158) do
+ActiveRecord::Schema.define(version: 2020_05_08_144845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_025158) do
     t.integer "idlocal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "valoracion"
   end
 
   create_table "comunas", force: :cascade do |t|
