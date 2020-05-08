b28dfac1c5d77946bc40598d6e164f1791247eca
Eduardo <evasqueztdev@protonmail.com>
2020-05-07 22:29:03 -0400

    Modelo y Controlador de Comentarios creados

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comentarios/new'
+  get 'comentarios/index'
+  get 'comentarios/show'
+  get 'comentarios/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
