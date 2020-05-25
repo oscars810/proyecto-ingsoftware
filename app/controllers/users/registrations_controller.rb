263d8e23d245c904f4951972c2021be4514d97da
oscars810 <oscar.carcamoz@uc.cl>
2020-05-24 23:33:16 -0400

    Problema comunas arreglado

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :obtener_comuna, :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,10 +10,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def create
-    super
-    user.commune = @comuna
-  end
+  # def create
+    # super
+  # end
 
   # GET /resource/edit
   # def edit
@@ -38,14 +37,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
-  def obtener_comuna
-    @comuna = Commune.find_by('nombre = ?', params[:commune])
-  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :commune_id])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
