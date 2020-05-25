bea81caa655caf38b0dd1b1fbc004be2dfca5009
oscars810 <oscar.carcamoz@uc.cl>
2020-05-25 00:00:50 -0400

    Registro actualizado

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,7 +41,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :commune_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :commune_id, :telefono, :edad])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
