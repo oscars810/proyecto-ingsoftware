46152411e878883d236761c483aa79c3098aa641
oscars810 <oscar.carcamoz@uc.cl>
2020-06-01 23:02:25 -0400

    merge con develop

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_05_30_041421) do
-=======
-ActiveRecord::Schema.define(version: 2020_05_30_031753) do
->>>>>>> develop
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
