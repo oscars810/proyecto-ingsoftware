5be033919b13eaa5b6821448768d7e252bf5d52d
oscars810 <oscar.carcamoz@uc.cl>
2020-06-04 22:36:01 -0400

    Fixing sintaxis

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_06_03_034745) do
-=======
 ActiveRecord::Schema.define(version: 2020_06_04_214230) do
->>>>>>> 06549ac1098af2c2b61652158dd796954a2cbe3c
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
